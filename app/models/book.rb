504f5940ca7e71b68a9fdee2d27b77c5f70462e7
zahra emad <zahraemad282@gmail.com>
2022-11-05 11:03:34 -0700

    commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
     belongs_to :user
     validates :year, uniqueness: {scope: :user_id}
+    #scope :year_2021, ->{where(year:2022)}
+    #scope :by_year,->(year) {where(year:year)}
 end
